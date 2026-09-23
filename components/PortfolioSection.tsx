a5bd414d923f8a85d8e3a95b7d5978b0ae7337a1
Eugene Zaiko <66bratz@gmail.com>
2026-09-23 13:46:42 +0300

    style:fix grid

--- components/PortfolioSection.tsx
+++ components/PortfolioSection.tsx
@@ -36,11 +36,7 @@ export default function PortfolioSection() {
           {portfolio.map((item, i) => (
             <div
               key={i}
-              className={`group relative overflow-hidden bg-[#C2BAA8] ${
-                i === 0
-                  ? "sm:col-span-2 aspect-video lg:aspect-[4/3]"
-                  : "aspect-[4/3]"
-              }`}
+              className={`group relative overflow-hidden bg-[#C2BAA8] aspect-[4/3]`}
             >
               <Image
                 src={item.img}
